0ef664cf6a3aac413d6aaab897c5f53f014da7cd
Dlong <joselargocha@gmail.com>
2024-05-10 12:06:05 -0500

    porfin baseurl

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,6 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.tceimports.com',
-  integrations: [tailwind(), react()]
+  integrations: [tailwind(), react()],
+  
 });
